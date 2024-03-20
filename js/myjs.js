d4767fd68f5bac54f7fbcdefcd4ba2f09a9f3cad
C0deXnoob <164237998+C0deXnoob@users.noreply.github.com>
2024-03-21 00:18:24 +0530

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#element', {
+    strings: ['Web Designer and...', 'Web Developer.'],
+    typeSpeed: 70,
+});
+function visitPage(){
+window.location='https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
+}
+function togglePopup() { 
+    const overlay = document.getElementById('popupOverlay'); 
+    overlay.classList.toggle('show'); 
+}
