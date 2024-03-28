ecd8c6cdf47df86266118310e30fa3ad35d00830
C0deXnoob <164237998+C0deXnoob@users.noreply.github.com>
2024-03-28 23:17:38 +0530

    Add files via upload

--- js/myjs.js
+++ js/myjs.js
@@ -3,9 +3,21 @@ var typed = new Typed('#element', {
     typeSpeed: 70,
 });
 function visitPage(){
-window.location='https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf';
+    window.open("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf", "_blank");
 }
 function togglePopup() { 
     const overlay = document.getElementById('popupOverlay'); 
     overlay.classList.toggle('show'); 
 }
+const headerEl = document.querySelector('.nav');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 200){
+        headerEl.classList.add('nav-scrolled');
+    }else if(window.scrollY <200)
+        headerEl.classList.remove('nav-scrolled');
+
+});
+function checkthis(){
+    window.open("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf", "_blank");
+    }
